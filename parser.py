359c6f24436d7e387fb94760025899a9c6476aaf
Eddie Schoute <eddieschoute@gmail.com>
2018-02-14 14:20:30 -0500

    Remove unneeded parser.py

--- parser.py
+++
@@ -1,7 +0,0 @@
-import tatsu.grammars
-
-quipper_model: tatsu.grammars.Grammar = tatsu.compile(open("grammar.ebnf").read())
-
-
-def parse(text: str):
-    return quipper_model.quipper_model.parse(text, start="start")
