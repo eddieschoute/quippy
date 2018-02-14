ebc76b78be9aad1d0f1f188f69d425e1458c50e5
Eddie Schoute <eddieschoute@gmail.com>
2018-02-14 14:08:38 -0500

    Fixes to grammar and moved to separate text file
    Added some more tests.

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
-from qiskit.dagcircuit import DAGCircuit
-import grammar
+import tatsu.grammars
+
+quipper_model: tatsu.grammars.Grammar = tatsu.compile(open("grammar.ebnf").read())
+
 
 def parse(text: str):
-    return grammar.quipper_model.parse(text, start="start")
+    return quipper_model.quipper_model.parse(text, start="start")
--- test_grammar.py
+++ test_grammar.py
@@ -1,18 +1,29 @@
 from unittest import TestCase
 from pathlib import Path
 
-import grammar
+import parser
+import tatsu
 
 ADDER_FILE = Path("resources") / "optimizer" / "Arithmetic_and_Toffoli" / "adder_8_before"
 
 class TestGrammar(TestCase):
     def test_iostatement(self):
         basic_text = """Inputs: 0:Qbit"""
-        parsed = grammar.quipper_model.parse(basic_text, start="iostatement")
+        parsed = parser.quipper_model.parse(basic_text, start="iostatement")
         self.assertEqual(['Inputs', ':', ['0', ':', 'Qbit'], []], parsed)
 
     def test_start(self):
         basic_text = """Inputs: 0:Qbit, 1:Qbit
         Outputs: 0:Qbit, 1:Qbit"""
-        parsed = grammar.quipper_model.parse(basic_text, start="start", asmodel=True)
+        parsed = parser.quipper_model.parse(basic_text, start="start", asmodel=True)
+        self.assertEqual(['Inputs', ':', ['0', ':', 'Qbit'], []], parsed)
+
+    def test_statement_qgate(self):
+        basic_text = '''QGate["not"](0) with controls=[+2] with nocontrol'''
+        parsed = parser.quipper_model.parse(basic_text, start="statement", asmodel=True)
+        self.assertEqual(['Inputs', ':', ['0', ':', 'Qbit'], []], parsed)
+
+    def test_statement_comment(self):
+        basic_text = '''Comment["ENTER: qft_big_endian"](0:"qs[0]", 1:"qs[1]")'''
+        parsed = parser.quipper_model.parse(basic_text, start="statement")
         self.assertEqual(['Inputs', ':', ['0', ':', 'Qbit'], []], parsed)
