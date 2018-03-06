3c70844e57ceabf911326c2dc87da01689d56730
Eddie Schoute <eddieschoute@gmail.com>
2018-03-06 10:30:16 -0500

    Include notice with sdist

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('.', ['LICENSE'])],  # Optional
+    data_files=[('.', ['LICENSE','NOTICE'])],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
